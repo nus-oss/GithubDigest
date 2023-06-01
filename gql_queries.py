22a3af3fc96645d27b532d398cbda47b61719026
ZQ <e0725180@u.nus.edu>
2023-06-02 05:19:05 +0800

    Debug test

--- gql_queries.py
+++ gql_queries.py
@@ -20,7 +20,6 @@ def run_queries(queries: list[str]) -> dict:
         "query": f"{{{','.join([q for q in queries])}}}"
     }
     response = requests.post(url, json=payload, headers=headers)
-    print(response.json())
     return response.json()["data"]
 
 def run_mutations(queries: list[str]) -> dict:
@@ -29,7 +28,6 @@ def run_mutations(queries: list[str]) -> dict:
     }
 
     response = requests.post(url, json=payload, headers=headers)
-    print(response.json())
     return response.json()["data"]
 
 class GithubQuery:
@@ -48,7 +46,6 @@ class GithubQuery:
         }
 
         response = requests.post(url, json=payload, headers=headers)
-        print(response.json())
         return response.json()["data"]
 
     def partial_query(self, **kwargs) -> str:
