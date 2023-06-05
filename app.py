265724dbd4f3acb2246694a307eec259e56ad1ee
ZQ <e0725180@u.nus.edu>
2023-06-05 15:18:28 +0800

    Lookup repo and local repo are fed into the app via environment variables. Therefore, it is not necessary to save them into the settings file as actions will provide the necessary values.
    Let's remove these field from the settings.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import json
 from digest_manager import DigestManager
 import os
 
+required_setting_fields = ["target_issue", "ignore_list"]
+
 lookup_repo = os.environ["GIT_REPO"]
 digest_dir = os.environ["DIGEST_SAVE_DIR"]
 curr_repo = os.environ["GITHUB_REPOSITORY"]
@@ -14,8 +16,6 @@ def create_digest_setting():
     os.makedirs(digest_dir, exist_ok=True)
     with open(savefile, 'w') as f:
         json.dump({
-            "target_repo": lookup_repo,
-            "my_repo": curr_repo,
             "target_issue": "",
             "ignore_list": []
         }, f, indent=4)
@@ -26,14 +26,21 @@ if not os.path.exists(savefile):
 with open(savefile, 'r') as f:
     try:
         setting = json.load(f)
-    except json.decoder.JSONDecodeError:
+        # ensure setting have all the required fields
+        for field in required_setting_fields:
+            if field not in setting:
+                print("Missing field detected, starting from scratch!")
+                raise KeyError(f"Missing field {field} in {savefile}")
+
+    except (json.decoder.JSONDecodeError ,KeyError) as e:
         create_digest_setting()
         with open(savefile, 'r') as f:
             setting = json.load(f)
 
+
 ql = DigestManager(
-    setting["target_repo"],
-    setting["my_repo"],
+    lookup_repo,
+    curr_repo,
     setting["target_issue"],
     ignore_numbers=setting["ignore_list"]
     )
